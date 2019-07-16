b3d022e481d5aadc6556e2fb5690ecf50e2a5509
Sreekanth Arikatla <sreekanth.arikatla@kitware.com>
2019-07-16 13:49:22 -0400

    ENH: Add user API
    1. Stop the physics simulation when converged
    2. Add more exception handling at initialization and API calls
    3. API: add force on a given vertebrae at a given location
    6. API: Get the position and orientation of the vertebrae (should let the user know if it is still converging)
    7. API: Clear all the forces
    8. Remove the force on a given vertebrae by the ID
    9. API: Allow the user to control the time step

--- physx/snippets/snippetjoint/SnippetJoint.cpp
+++ physx/snippets/snippetjoint/SnippetJoint.cpp
@@ -36,42 +36,62 @@
 
 #include <ctype.h>
 #include <iostream>
+#include <limits>
+#include <algorithm>
 
 #include "PxPhysicsAPI.h"
 
 #include "../snippetcommon/SnippetPrint.h"
 #include "../snippetcommon/SnippetPVD.h"
 #include "../snippetutils/SnippetUtils.h"
+#include "../../include/extensions/PxRigidBodyExt.h"
 
 #define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
 #include "tiny_obj_loader.h"
 
 using namespace physx;
 
+// PhysX
 PxDefaultAllocator		gAllocator;
 PxDefaultErrorCallback	gErrorCallback;
-
 PxFoundation*			gFoundation = NULL;
 PxPhysics*				gPhysics	= NULL;
-
 PxDefaultCpuDispatcher*	gDispatcher = NULL;
 PxScene*				gScene		= NULL;
-
 PxMaterial*				gMaterial	= NULL;
-
 PxPvd*                  gPvd        = NULL;
-
-// apply forces on these actors
-PxRigidDynamic* actor1 = NULL;
-
 PxCooking*				gCooking = NULL;
 
-PxRigidDynamic* gRigidDynaMeshActor;
+bool paused = false;                        ///> Tracks if the simulation is paused or not
+bool firstIteration = true;                 ///> True if before the first simulation iteration
+bool includeGroundPlane = true;             ///> Include non-functional ground plane or not
+unsigned int frameCount = 0;                ///> Stores the simulation frame number
+std::vector<PxRigidDynamic*> vertebrae;     ///> Vector of all the dynamic actors created for vertebrae
+float poseDataPrev[168];                    ///> Stores the pose of the spine from last simulation frame
+float poseDataCurrent[168];                 ///> Stores the pose of the spine in current simulation frame
 
-bool paused = true;
-bool includeGroundPlane = true;
-unsigned int frameCount = 0;
+///
+/// \brief data structure to hold the force specification
+/// stores the amount of force, the vertebrae number and 
+/// the position (w.r.t local frame) where the force should be applied
+///
+struct vertebraeForce
+{
+    PxVec3 force;
+    PxVec3 position;
+    PxU32 vertebraeNumber;
 
+    vertebraeForce(const PxU32 num, const PxVec3 f, const PxVec3 p) :
+        vertebraeNumber(num), force(f), position(p) {}
+};
+std::vector<vertebraeForce> vertebraeForces; ///>  All the external forces to be applied is stored here
+
+// user parameters
+float convergenceTol = 0.5e-3; ///> Tolerance against which the convergence of the spine pose will be checked
+float timeStepSize = 1. / 400; ///> Time step used for the simulation
+
+
+/// Store the name of the external files of simplistic vertebrae models registered to the 3D spine
 std::vector<std::string> LseriesFiles = { "L01M.obj", "L02M.obj", "L03M.obj", "L04M.obj", "L05M.obj" };
 
 std::vector<std::string> TseriesFiles = { "T01M.obj", "T02M.obj", "T03M.obj", "T04M.obj", "T05M.obj",
@@ -81,10 +101,17 @@ std::vector<std::string> TseriesFiles = { "T01M.obj", "T02M.obj", "T03M.obj", "T
 std::vector<std::string> CseriesFiles = { "C01M.obj", "C02M.obj", "C03M.obj", "C04M.obj", "C05M.obj",
                                         "C06M.obj", "C07M.obj" };
 
-// Spine angle limits
+/// Spine angle limits in Cartesian axes in the local frame of the joint
 std::vector<float> xAngularLimits = { 10., 5., 7.5, 10., 10., 8., 4.5, 2., 2., 2., 2., 2., 2.5, 3., 3., 3., 4., 6., 6., 6., 7., 7., 8. };
 std::vector<float> yAngularLimits = { 4., 10., 11., 11., 8., 7., 4., 5., 6., 5., 6., 6., 6., 6., 6., 6., 7., 9., 8., 6., 6., 8., 6.};
 std::vector<float> zAngularLimits = { 0., 3., 6.5, 6.5, 6.5, 6., 2., 9., 8., 8., 8., 8., 7., 7., 6., 4., 2., 2., 2., 2., 2., 2., 2. };
+
+// forward deceleration
+void addForceOnVertebrae(const unsigned int vertebraeNum, const PxVec3& force, const PxVec3& pos);
+void addForceOnVertebrae(const unsigned int vertebraeNum,
+    const float fX, const float fY, const float fZ,
+    const float pX, const float pY, const float pZ);
+void removeForcesOnVertebrae(const unsigned int vertebraeNum);
 
 #define REPORT_INIT_ERROR_IFANY(OBJECT, CLBK, MESG) \
 if(!OBJECT) CLBK.reportError(physx::PxErrorCode::eINTERNAL_ERROR, MESG, __FILE__, __LINE__);
@@ -115,6 +142,7 @@ void loadObjFile(const std::string &filename, std::vector<PxVec3>& vertices, std
 
     if (!ret) 
     {
+        printf("ERROR loading %s\n", filename);
         exit(1);
     }
 
@@ -216,87 +244,65 @@ typedef PxJoint* (*JointCreateFunction)(PxRigidActor* a0, const PxTransform& t0,
 ///
 /// \brief create a spine model with custom D6 links between the vertebrae
 ///
-void createRealSpineModel(const PxU32 length, PxTransform& offset)
+void createRealSpineModel(PxTransform& offset)
 {
-    std::vector<PxTransform> vertibraeTransforms;
-    std::vector<PxVec3> vertibraCenters(length, PxVec3(0., 0., 0.));
-    std::vector<PxRigidActor*> vertibrae;
+    const PxU32 length = 24;
+    std::vector<PxTransform> vertebraeTransforms;
+    std::vector<PxVec3> vertebraCenters(length, PxVec3(0., 0., 0.));
 
-    std::vector<std::string> vertibraeName;
-    if (length == 12)
-    {
-        vertibraeName.insert(vertibraeName.begin(), TseriesFiles.begin(), TseriesFiles.end());
-    }    
-    else if (length == 17)
-    {
-        vertibraeName.insert(vertibraeName.begin(), TseriesFiles.begin(), TseriesFiles.end());
-        vertibraeName.insert(vertibraeName.end(), LseriesFiles.begin(), LseriesFiles.end());
-    }
-    else if (length == 24)
-    {
-        vertibraeName.insert(vertibraeName.begin(), CseriesFiles.begin(), CseriesFiles.end());
-        vertibraeName.insert(vertibraeName.end(), TseriesFiles.begin(), TseriesFiles.end());
-        vertibraeName.insert(vertibraeName.end(), LseriesFiles.begin(), LseriesFiles.end());
-    }
-    else
-    {
-        std::cout << "Supply right number of vertibrae";
-        exit(1);
-    }
+    std::vector<std::string> vertebraeName;
 
-    // Create vertibrae
-    for (PxU32 i = 0; i < vertibraeName.size(); ++i)
+    vertebraeName.insert(vertebraeName.begin(), CseriesFiles.begin(), CseriesFiles.end());
+    vertebraeName.insert(vertebraeName.end(), TseriesFiles.begin(), TseriesFiles.end());
+    vertebraeName.insert(vertebraeName.end(), LseriesFiles.begin(), LseriesFiles.end());
+
+    // Create vertebrae
+    for (PxU32 i = 0; i < vertebraeName.size(); ++i)
     {
         std::vector<PxVec3> vertices;
         std::vector <PxU32> indices;
 
         // Load the input file
-        loadObjFile(std::string("objs/" + vertibraeName[i]), vertices, indices); 
+        loadObjFile(std::string("objs/" + vertebraeName[i]), vertices, indices); 
             
         // Extract the transform and half lengths from the loaded model
         // Use the resulting parameters to create a box geometry
-        vertibraeTransforms.push_back(offset*computeTransformOfBox(vertices, indices));
+        vertebraeTransforms.push_back(offset*computeTransformOfBox(vertices, indices));
         auto boxGeo = PxBoxGeometry(computeBoxHalfLengths(vertices));
 
         // Create a dynamic model with the box geometry
-        PxRigidDynamic* dynaObj = PxCreateDynamic(*gPhysics, vertibraeTransforms[i], boxGeo, *gMaterial, 1.0f);
-
-        // Add a force on a particular actor
-        if (i == 10)
-        { 
-            actor1 = dynaObj; 
-        }
+        PxRigidDynamic* dynaObj = PxCreateDynamic(*gPhysics, vertebraeTransforms[i], boxGeo, *gMaterial, 1.0f);
         
         dynaObj->setMass(0.2);
         dynaObj->setActorFlag(PxActorFlag::eDISABLE_GRAVITY, true); // disable gravity
                 
-        vertibrae.push_back(dynaObj);
+        vertebrae.push_back(dynaObj);
         
-        gScene->addActor(*vertibrae[i]);         
+        gScene->addActor(*vertebrae[i]);         
 
-        // Compute center center points of the vertibrae
+        // Compute center center points of the vertebrae
         for (auto& v : vertices)
         {
-            vertibraCenters[i] += v;
+            vertebraCenters[i] += v;
         }        
-        vertibraCenters[i] /= 8.;
+        vertebraCenters[i] /= 8.;
     }
 
     // Create joints in the local frame
     for (PxU32 i = 0; i < length - 1; ++i)
     {
-        const PxReal zOffset = (vertibraCenters[i + 1] - vertibraCenters[i]).magnitude()*0.5;
-        createDampedD6(vertibrae[i], PxTransform(PxVec3(0., 0., -zOffset)), vertibrae[i + 1], PxTransform(PxVec3(0., 0., zOffset)), 
+        const PxReal zOffset = (vertebraCenters[i + 1] - vertebraCenters[i]).magnitude()*0.5;
+        createDampedD6(vertebrae[i], PxTransform(PxVec3(0., 0., -zOffset)), vertebrae[i + 1], PxTransform(PxVec3(0., 0., zOffset)), 
                        xAngularLimits[i], yAngularLimits[i], zAngularLimits[i]);
     }
 
     // create end joints (with angular limits of the nearest joint)
-    const PxReal zOffset = (vertibraCenters[1] - vertibraCenters[0]).magnitude()*0.5;
-    createDampedD6(NULL, vertibraeTransforms[0] * PxTransform(PxVec3(0., 0., 0)), vertibrae[0], PxTransform(PxVec3(0., 0., -0)), 
+    const PxReal zOffset = (vertebraCenters[1] - vertebraCenters[0]).magnitude()*0.5;
+    createDampedD6(NULL, vertebraeTransforms[0] * PxTransform(PxVec3(0., 0., 0)), vertebrae[0], PxTransform(PxVec3(0., 0., -0)), 
                    xAngularLimits[0], yAngularLimits[0], zAngularLimits[0]);
     
-    const PxReal zOffset2 = (vertibraCenters[length-1] - vertibraCenters[length-2]).magnitude()*0.5;
-    createDampedD6(vertibrae[length - 1], PxTransform(PxVec3(0., 0., 0)), NULL, vertibraeTransforms[length-1] * PxTransform(PxVec3(0., 0., -0)), 
+    const PxReal zOffset2 = (vertebraCenters[length-1] - vertebraCenters[length-2]).magnitude()*0.5;
+    createDampedD6(vertebrae[length - 1], PxTransform(PxVec3(0., 0., 0)), NULL, vertebraeTransforms[length-1] * PxTransform(PxVec3(0., 0., -0)), 
                    xAngularLimits[length - 2], yAngularLimits[length - 2], zAngularLimits[length - 2]);
 }
 
@@ -321,7 +327,7 @@ void initPhysics(bool /*interactive*/)
     REPORT_INIT_ERROR_IFANY(gCooking, gErrorCallback, "PxCreateCooking failed!!")
 
 	PxSceneDesc sceneDesc(gPhysics->getTolerancesScale());
-    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);// PxVec3(0.0f, -9.81f, 0.0f);
+    sceneDesc.gravity = PxVec3(0.0f, -9.81f, 0.0f);
 	gDispatcher = PxDefaultCpuDispatcherCreate(2);
 	sceneDesc.cpuDispatcher	= gDispatcher;
 	sceneDesc.filterShader	= PxDefaultSimulationFilterShader;
@@ -345,17 +351,126 @@ void initPhysics(bool /*interactive*/)
     }
 
     // Create a spine model
-    createRealSpineModel(24, PxTransform(PxVec3(0.0f, 40.0f, 0.0f)));   
+    createRealSpineModel(PxTransform(PxVec3(0.0f, 40.0f, 0.0f)));   
+
+    // add sample force
+    addForceOnVertebrae(10, PxVec3(-1.2e2f, 0.0f, 0.0f), PxVec3(0.f, 0.f, 0.f));
 }
 
-void transferGlobalPose(PxRigidDynamic* sourceActor, PxRigidStatic* destinationActor, PxTransform& offset = PxTransform(PxIdentity))
-{    
-    if (!sourceActor || !destinationActor) 
-    { 
+///
+/// \brief Get the pose of all the 24 vertebrae
+///
+void getSpinePose(float* poseData)
+{
+    unsigned int count = 0;
+    for (const auto& v : vertebrae)
+    {
+        const auto t = v->getGlobalPose();
+        poseData[count++] = t.p.x;
+        poseData[count++] = t.p.y;
+        poseData[count++] = t.p.z;
+
+        poseData[count++] = t.q.w;
+        poseData[count++] = t.q.x;
+        poseData[count++] = t.q.y;
+        poseData[count++] = t.q.z;
+    }
+}
+
+///
+/// \brief Set the time step size
+///
+void setTimeStepSize(const float timeStep)
+{
+    timeStepSize = timeStep;
+}
+
+///
+/// \brief Check if the spine pose has converged with a preset tolerance
+///
+bool poseConverged()
+{
+    getSpinePose(poseDataCurrent);
+
+    float norm = 0.;
+    for (int i = 0; i < 168; ++i)
+    {
+        norm += std::abs(poseDataCurrent[i] - poseDataPrev[i]);
+    }
+    //printf("norm: %f\n", norm);
+
+    if (norm > convergenceTol)
+    {
+        for (int i = 0; i < 168; ++i)
+        {
+            poseDataPrev[i] = poseDataCurrent[i];
+        }
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+///
+/// \brief Add a force at a given vertebrae
+/// \param vertebraeNum vertebrae number [0, 23]
+/// \param force force to be applied on the vertebrae
+/// \param force Position w.r.t local vertebrae frame where the force will be applied
+///
+void addForceOnVertebrae(const unsigned int vertebraeNum, const PxVec3& force, const PxVec3& pos)
+{
+    if (vertebraeNum > 23)
+    {
+        printf("Warning: @addForceOnVertebrae - The Vertebrae number supplied is not valid!");
+        return;
+    }
+    vertebraeForces.push_back(vertebraeForce(vertebraeNum, force, pos));
+}
+
+void addForceOnVertebrae(const unsigned int vertebraeNum, 
+                         const float fX, const float fY, const float fZ,
+                         const float pX, const float pY, const float pZ)
+{
+    addForceOnVertebrae(vertebraeNum, PxVec3(fX, fY, fZ), PxVec3(pX, pY, pZ));
+}
+
+///
+/// \brief Clear all the forces on all the vertebrae
+///
+void clearAllForces()
+{
+    vertebraeForces.clear();
+}
+
+///
+/// \brief Remove all the forces on a certain vertebrae
+/// \param vertebraeNum vertebrae number [0, 23]
+///
+void removeForcesOnVertebrae(const unsigned int vertebraeNum)
+{
+    if (vertebraeNum > 23)
+    {
+        printf("Warning: @clearForceOnVertebrae - The Vertebrae number supplied is not valid!");
         return;
     }
 
-    destinationActor->setGlobalPose(offset*sourceActor->getGlobalPose());
+    vertebraeForces.erase(std::remove_if(vertebraeForces.begin(), vertebraeForces.end(), [&vertebraeNum](const vertebraeForce& f)
+    {
+        return (f.vertebraeNumber == vertebraeNum) ? true : false;
+    }));
+}
+
+///
+/// \brief Apply the all the specified forces
+///
+void applyForces()
+{
+    for (const auto& f : vertebraeForces)
+    {
+        PxRigidBodyExt::addForceAtLocalPos(*vertebrae[f.vertebraeNumber], f.force, f.position, PxForceMode::eFORCE, true);
+    }
 }
 
 ///
@@ -363,20 +478,27 @@ void transferGlobalPose(PxRigidDynamic* sourceActor, PxRigidStatic* destinationA
 ///
 void stepPhysics(bool /*interactive*/)
 {
-    if (!paused && actor1)
+    if (!paused)
     {
-        if (frameCount > 60000 && frameCount < 90000)
-        {
-            actor1->addForce(PxVec3(-1.2e2f, 0.0f, 0.0f), PxForceMode::eFORCE, true);
+        if (frameCount > 2000)
+        {            
+            applyForces();
         }        
         frameCount++;
     }
 
+    if (frameCount > 3000 && !firstIteration && !paused && poseConverged())
+    {
+        paused = true;
+        printf("Converged!\n");
+    }
+
     if (!paused)
     {
-        gScene->simulate(1.0f / 400.0f);
+        gScene->simulate(timeStepSize);
         gScene->fetchResults(true);
-        //paused = !paused;
+
+        firstIteration = false;
     }
 }
 
@@ -407,7 +529,7 @@ void keyPress(unsigned char key, const PxTransform& camera)
 	switch(toupper(key))
 	{
     case ' ':
-        paused=!paused;
+        paused = !paused;
         break;
 	}
 }
@@ -418,9 +540,9 @@ int snippetMain(int, const char*const*)
 	extern void renderLoop();
 	renderLoop();
 #else
-	static const PxU32 frameCount = 100;
+	static const PxU32 frameNum = 100;
 	initPhysics(false);
-	for(PxU32 i=0; i<frameCount; i++)
+	for(PxU32 i=0; i< frameNum; i++)
 		stepPhysics(false);
 	cleanupPhysics(false);
 #endif
